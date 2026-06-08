7dc81de1df0be314d6564efdde30d8917a3d6f51
Drew Shoaf <drewshoaf@Drews-MacBook-Air.local>
2026-06-08 15:43:05 -0400

    Update Proveria formula to v0.1.1

--- Formula/proveria.rb
+++ Formula/proveria.rb
@@ -4,15 +4,15 @@ class Proveria < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/proveria/proveria-cli/releases/download/proveria-cli-v0.1.0/proveria-aarch64-apple-darwin.tar.gz"
-      sha256 "b0c6dcdd7555d4c215a0d1d92a392fe6e5da269f1a5a8c4e8838644a6fa87567"
+      url "https://github.com/proveria/proveria-cli/releases/download/proveria-cli-v0.1.1/proveria-aarch64-apple-darwin.tar.gz"
+      sha256 "ca36da61ba9bb14a30c3a2789e01c65ad99920a2cbd75c6d6f67bc59d75b8c7f"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/proveria/proveria-cli/releases/download/proveria-cli-v0.1.0/proveria-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9f9b7a3804f35cdbdc9fbcb3ae569394f76dcb31325623d503acfcbaeb1548c9"
+      url "https://github.com/proveria/proveria-cli/releases/download/proveria-cli-v0.1.1/proveria-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "14a84416e34fafce5cb07c10006bc9d09c07a2c8bc391e4d298db5f481a48730"
     end
   end
 
