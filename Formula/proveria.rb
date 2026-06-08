ec8ba708623e39fa663e9a9cecfd1bb3edb49257
Drew Shoaf <drewshoaf@Drews-MacBook-Air.local>
2026-06-08 07:42:01 -0400

    Fix formula platform blocks

--- Formula/proveria.rb
+++ Formula/proveria.rb
@@ -1,16 +1,19 @@
 class Proveria < Formula
-  desc "Proveria CLI for local hashing, attestations, receipts, and API workflows"
+  desc "CLI for local hashing, attestations, receipts, and API workflows"
   homepage "https://github.com/proveria/proveria"
-  version "0.1.0"
 
   on_macos do
-    url "https://github.com/proveria/proveria/releases/download/proveria-cli-v0.1.0/proveria-aarch64-apple-darwin.tar.gz"
-    sha256 "009d2f6555831c407bb12c2f8e0fbdc52c466453457992af0ee18c3b35f494e7"
+    on_arm do
+      url "https://github.com/proveria/proveria/releases/download/proveria-cli-v0.1.0/proveria-aarch64-apple-darwin.tar.gz"
+      sha256 "009d2f6555831c407bb12c2f8e0fbdc52c466453457992af0ee18c3b35f494e7"
+    end
   end
 
   on_linux do
-    url "https://github.com/proveria/proveria/releases/download/proveria-cli-v0.1.0/proveria-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "4d15505c5bf1dfe442a3db151f5519c03549a197ac05f6900472937c39e22d12"
+    on_intel do
+      url "https://github.com/proveria/proveria/releases/download/proveria-cli-v0.1.0/proveria-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "4d15505c5bf1dfe442a3db151f5519c03549a197ac05f6900472937c39e22d12"
+    end
   end
 
   def install
