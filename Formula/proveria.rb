803f22bd91209eae2ac31201ff37a4a70033d06d
Drew Shoaf <drewshoaf@Drews-MacBook-Air.local>
2026-06-08 08:03:41 -0400

    Point formula to public CLI repo

--- Formula/proveria.rb
+++ Formula/proveria.rb
@@ -1,17 +1,17 @@
 class Proveria < Formula
   desc "CLI for local hashing, attestations, receipts, and API workflows"
-  homepage "https://github.com/proveria/proveria"
+  homepage "https://github.com/proveria/proveria-cli"
 
   on_macos do
     on_arm do
-      url "https://github.com/proveria/proveria/releases/download/proveria-cli-v0.1.0/proveria-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/proveria/proveria-cli/releases/download/proveria-cli-v0.1.0/proveria-aarch64-apple-darwin.tar.gz"
       sha256 "009d2f6555831c407bb12c2f8e0fbdc52c466453457992af0ee18c3b35f494e7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/proveria/proveria/releases/download/proveria-cli-v0.1.0/proveria-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/proveria/proveria-cli/releases/download/proveria-cli-v0.1.0/proveria-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "4d15505c5bf1dfe442a3db151f5519c03549a197ac05f6900472937c39e22d12"
     end
   end
